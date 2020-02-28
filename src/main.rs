5e24b78ffe38ccb310b72cdec8ae28544913998b
Andrew Jones <ascjones@gmail.com>
2020-02-28 13:57:48 +0000

    Update description

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() -> sc_cli::Result<()> {
 		version: env!("CARGO_PKG_VERSION"),
 		executable_name: "paracon",
 		author: "Parity Team <admin@parity.io>",
-		description: "Paracon Smart Contracts chain Client Node",
+		description: "Paracon Smart Contracts Chain Client Node",
 		support_url: "https://github.com/paritytech/paracon-node/issues/new",
 		copyright_start_year: 2020,
 	};
