e1ef4337d9f020fe85fbb2f62c74d2d079c92601
Andrew Jones <ascjones@gmail.com>
2020-04-17 11:17:37 +0100

    Update to substrate alpha.6 and latest template layout (#8)
    * Upgrade to substrate alpha.5 crates
    
    * Start upgrading to alpha.6, node dir
    
    * Update node files to match template
    
    * Update runtime
    
    * Fix up remaining errors

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -4,28 +4,17 @@ use paracon_runtime::{
 	SudoConfig, SystemConfig, WASM_BINARY, Signature,
 	ContractsConfig, MILLICENTS,
 };
-use sp_consensus_aura::sr25519::{AuthorityId as AuraId};
-use grandpa_primitives::{AuthorityId as GrandpaId};
-use sc_service;
+use sp_consensus_aura::sr25519::AuthorityId as AuraId;
+use sp_finality_grandpa::AuthorityId as GrandpaId;
 use sp_runtime::traits::{Verify, IdentifyAccount};
+use sc_service::ChainType;
 use hex_literal::hex;
 
 // Note this is the URL for the telemetry server
 //const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
 
 /// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
-pub type ChainSpec = sc_service::ChainSpec<GenesisConfig>;
-
-/// The chain specification option. This is expected to come in from the CLI and
-/// is little more than one of a number of alternatives which can easily be converted
-/// from a string (`--chain=...`) into a `ChainSpec`.
-#[derive(Clone, Debug)]
-pub enum Alternative {
-	/// Whatever the current runtime is, with just Alice as an auth.
-	Development,
-	/// Paracon Test net 1
-	TestNet1,
-}
+pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig>;
 
 /// Helper function to generate a crypto pair from seed
 pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
@@ -44,7 +33,7 @@ pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId where
 }
 
 /// Helper function to generate an authority key for Aura
-pub fn get_authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
+pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
 	(
 		get_from_seed::<AuraId>(s),
 		get_from_seed::<GrandpaId>(s),
@@ -69,58 +58,52 @@ pub fn testnet_root() -> AccountId {
 	hex!("baa78c7154c7f82d6d377177e20bcab65d327eca0086513f9964f5a0f6bdad56").into()
 }
 
-impl Alternative {
-	/// Get an actual chain config from one of the alternatives.
-	pub(crate) fn load(self) -> Result<ChainSpec, String> {
-		Ok(match self {
-			Alternative::Development => ChainSpec::from_genesis(
-				"Development",
-				"dev",
-				|| testnet_genesis(vec![
-					get_authority_keys_from_seed("Alice"),
-				],
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
-				vec![
-					get_account_id_from_seed::<sr25519::Public>("Alice"),
-					get_account_id_from_seed::<sr25519::Public>("Bob"),
-					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-				],
-				true),
-				vec![],
-				None,
-				None,
-				None,
-				None
-			),
-			Alternative::TestNet1 => ChainSpec::from_genesis(
-				"Paracon Testnet 1",
-				"paracon_testnet1",
-				|| testnet_genesis(
-					testnet_authorities(),
-					testnet_root(),
-					vec![testnet_root()],
-					true,
-				),
-				vec![
-					"/ip4/35.233.19.96/tcp/30333/p2p/QmNvYhAZSBtahCqCXznYiq8e24Yes1GraPFYCc3DyA5f3z".to_string(),
-					"/ip4/35.205.110.21/tcp/30333/p2p/QmPKFc9B2oeQFc5oxbNsRENwSYibzzafKmcHs9wBZCJH4U".to_string(),
-				],
-				None,
-				Some("prc"),
-				None,
-				None
-			),
-		})
-	}
+pub fn development_config() -> ChainSpec {
+	ChainSpec::from_genesis(
+		"Development",
+		"dev",
+		ChainType::Development,
+		|| testnet_genesis(vec![
+			authority_keys_from_seed("Alice"),
+		],
+						   get_account_id_from_seed::<sr25519::Public>("Alice"),
+						   vec![
+							   get_account_id_from_seed::<sr25519::Public>("Alice"),
+							   get_account_id_from_seed::<sr25519::Public>("Bob"),
+							   get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+							   get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+						   ],
+						   true),
+		vec![],
+		None,
+		None,
+		None,
+		None
+	)
+}
 
-	pub(crate) fn from(s: &str) -> Option<Self> {
-		match s {
-			"dev" => Some(Alternative::Development),
-			"" | "local" => Some(Alternative::TestNet1),
-			_ => None,
-		}
-	}
+pub fn testnet_config() -> ChainSpec {
+	ChainSpec::from_genesis(
+		"Paracon Testnet 1",
+		"paracon_testnet1",
+		ChainType::Live,
+		|| testnet_genesis(
+			testnet_authorities(),
+			testnet_root(),
+			vec![testnet_root()],
+			true,
+		),
+		vec![
+			"/ip4/35.233.19.96/tcp/30333/p2p/QmNvYhAZSBtahCqCXznYiq8e24Yes1GraPFYCc3DyA5f3z".parse()
+				.expect("MultiaddrWithPeerId"),
+			"/ip4/35.205.110.21/tcp/30333/p2p/QmPKFc9B2oeQFc5oxbNsRENwSYibzzafKmcHs9wBZCJH4U".parse()
+				.expect("MultiaddrWithPeerId"),
+		],
+		None,
+		Some("prc"),
+		None,
+		None
+	)
 }
 
 fn testnet_genesis(
@@ -154,11 +137,4 @@ fn testnet_genesis(
 		}),
 		contracts: Some(contracts_config),
 	}
-}
-
-pub fn load_spec(id: &str) -> Result<Option<ChainSpec>, String> {
-	Ok(match Alternative::from(id) {
-		Some(spec) => Some(spec.load()?),
-		None => None,
-	})
 }
