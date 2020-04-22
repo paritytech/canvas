8466dee2782f84ce07f709b1f15e0420340ee454
Ricardo Rius <ricardo@parity.io>
2020-04-22 11:16:02 +0200

    Fix indentation2

--- node/src/service.rs
+++ node/src/service.rs
@@ -66,12 +66,12 @@ macro_rules! new_full_start {
 				Ok(import_queue)
 			})?
 			.with_rpc_extensions(|builder| -> Result<IoHandler<sc_rpc::Metadata>, _> {
-					let handler = pallet_contracts_rpc::Contracts::new(builder.client().clone());
-					let delegate = pallet_contracts_rpc::ContractsApi::to_delegate(handler);
+				let handler = pallet_contracts_rpc::Contracts::new(builder.client().clone());
+				let delegate = pallet_contracts_rpc::ContractsApi::to_delegate(handler);
 
-					let mut io = IoHandler::default();
-					io.extend_with(delegate);
-					Ok(io)
+				let mut io = IoHandler::default();
+				io.extend_with(delegate);
+				Ok(io)
 			})?;
 
 		(builder, import_setup, inherent_data_providers)
