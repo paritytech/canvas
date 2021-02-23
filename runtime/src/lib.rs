b9ae164c676755900452364c320a6c2c65b6565d
Andrew Jones <ascjones@gmail.com>
2021-02-23 10:21:45 +0000

    Increment spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -96,7 +96,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("canvas"),
 	impl_name: create_runtime_str!("canvas"),
 	authoring_version: 1,
-	spec_version: 8,
+	spec_version: 9,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
