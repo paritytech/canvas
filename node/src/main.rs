891711fe9bca3be348456ed257ce2016713e4e6d
Michael Mueller <mich@elmueller.net>
2021-04-26 17:59:51 +0200

    Strip unnecessary stuff

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,13 +1,1 @@
-//! Substrate Node CLI library.
-#![warn(missing_docs)]
-
-mod chain_spec;
-#[macro_use]
-mod service;
-mod cli;
-mod command;
-mod rpc;
-
-fn main() -> sc_cli::Result<()> {
-	command::run()
-}
+fn main() {}
